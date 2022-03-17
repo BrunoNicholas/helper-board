622f2f9cbdf622b7b7370eaddc9ba9ec3c5ca72c
BrunoNicholas <sbnibro256@gmail.com>
2022-03-17 09:09:48 +0300

    adding auth login and register

--- backend/app/controllers/auth_controller.py
+++ backend/app/controllers/auth_controller.py
@@ -1,38 +1,39 @@
-from flask import Flask, request, jsonify, make_response
-import uuid  # for public id
-from werkzeug.security import generate_password_hash, check_password_hash
-# imports for PyJWT authentication
+from flask import request, jsonify
 import jwt
-from datetime import datetime, timedelta
 from functools import wraps
 from ..models.user import User
-
 from app import app
 
 
-# decorator for verifying the JWT
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-        # jwt is passed in the request header
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-        # return 401 if token is not passed
-        if not token:
-            return jsonify({'message': 'Token is missing !!'}), 401
+class UserAuth:
+    # decorator for verifying the JWT
+    def token_required(self):
+        @wraps(self)
+        def decorated(*args, **kwargs):
+            token = None
 
-        try:
-            # decoding the payload to fetch the stored details
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-            current_user = User.query \
-                .filter_by(public_id=data['public_id']) \
-                .first()
-        except:
-            return jsonify({
-                'message': 'Token is invalid !!'
-            }), 401
-        # returns the current logged in users contex to the routes
-        return f(current_user, *args, **kwargs)
+            # jwt is passed in the request header
+            if 'x-access-token' in request.headers:
+                token = request.headers['x-access-token']
 
-    return decorated
+            # return 401 if token is not passed
+            if not token:
+                return jsonify({
+                    'error': 'Unauthenticated!',
+                    'message': 'You are currently not logged in'
+                }), 401
+
+            try:
+                # decoding the payload to fetch the stored details
+                data = jwt.decode(token, app.config['SECRET_KEY'])
+                current_user = User.query.filter_by(public_id=data['public_id']).first()
+
+            except ValueError:
+                return jsonify({
+                    'message': 'Invalid token applied!'
+                }), 401
+
+            # returns the current logged-in users' context to the routes
+            return self
+
+        return decorated
--- backend/app/views/routes.py
+++ backend/app/views/routes.py
@@ -1,4 +1,11 @@
-from flask import Blueprint, jsonify, request, abort
+from flask import Blueprint, jsonify, request, make_response
+import uuid
+import jwt
+from datetime import datetime, timedelta
+from werkzeug.security import generate_password_hash, check_password_hash
+from ..controllers.auth_controller import UserAuth
+from ..models.user import User
+from app import app, db
 
 JSON_MIME_TYPE = 'application/json'
 
@@ -13,3 +20,136 @@ def index():
         'status': 200
     }
     return jsonify(data), 200
+
+
+# this route sends back list of users
+@system_app.route('/user', methods=['GET'])
+@UserAuth.token_required
+def get_all_users():
+    # querying the database for all the entries in it
+    users = User.query.all()
+
+    # converting the query objects to list of jsons
+    output = []
+    for user in users:
+        # appending the user data json to the response list
+        output.append({
+            'public_id': user.public_id,
+            'name': user.name,
+            'email': user.email
+        })
+
+    return jsonify({'users': output})
+
+
+@system_app.route('/login', methods=['GET'])
+def get_login():
+    return jsonify({
+        'message': 'Use POST for this route'
+    }), 400
+
+
+# route for logging user in
+@system_app.route('/login', methods=['POST'])
+def login():
+    # creates dictionary of form data
+    data = request.form
+    errors = []
+
+    if not data:
+        errors.append({'input': ['No content is submitted']})
+
+    if not data.get('email'):
+        errors.append({'email': ['Your email is required']})
+
+    if not data.get('password'):
+        errors.append({'password': ['Your password is required']})
+
+    if not data or not data.get('email') or not data.get('password'):
+        return jsonify({
+            'errors': errors
+        }), 400
+
+    user = User.query.filter_by(email=data.get('email')).first()
+
+    if not user:
+        return jsonify({
+            'errors': [
+                {
+                    'email': ['No user found with that email, create a new account']
+                }
+            ]
+        }), 404
+
+    if check_password_hash(user.password, data.get('password')):
+        # generates the JWT Token
+        token = jwt.encode({
+            'public_id': user.public_id,
+            'exp': datetime.utcnow() + timedelta(minutes=30)
+        }, app.config['SECRET_KEY'])
+
+        return make_response(jsonify({'token': token.decode('UTF-8')}), 201)
+
+    return jsonify({
+        'errors': [
+            {
+                'password': ['Invalid password provided. Please try again']
+            }
+        ]
+    }), 404
+
+
+# signup route
+@system_app.route('/signup', methods=['POST'])
+def signup():
+    # creates a dictionary of the form data
+    data = request.form
+    errors = []
+
+    if not data.get('name'):
+        errors.append({'name': ['Your name is required']})
+
+    if not data.get('email'):
+        errors.append({'email': ['Your email is required']})
+
+    if not data.get('password'):
+        errors.append({'password': ['Your password is required']})
+
+    if not data or not data.get('name') or not data.get('email') or not data.get('password'):
+        return jsonify({
+            'errors': errors
+        }), 400
+
+    name, email = data.get('name'), data.get('email')
+    password = data.get('password')
+    is_admin = data.get('is_admin')
+    is_dev = data.get('is_dev')
+    is_student = data.get('is_student')
+    status = data.get('status')
+    created_at = datetime.now()
+    updated_at = datetime.now()
+
+    # checking for existing user
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        # database ORM object
+        user = User(
+            public_id=str(uuid.uuid4()),
+            name=name,
+            email=email,
+            password=generate_password_hash(password),
+            is_admin=is_admin,
+            is_dev=is_dev,
+            is_student=is_student,
+            status=status,
+            created_at=created_at,
+            updated_at=updated_at
+        )
+        # insert user
+        db.session.add(user)
+        db.session.commit()
+        return make_response('Successfully registered.', 201)
+
+    else:
+        return make_response('User already exists. Please Log in.', 202)
