52b9995dafc2b3779efad171caeb3b8ef1c534a3
BrunoNicholas <sbnibro256@gmail.com>
2022-03-20 01:49:54 +0300

    adding service content

--- frontend/src/services/NotificationService.js
+++ frontend/src/services/NotificationService.js
@@ -5,19 +5,48 @@ const API_URL = Endpoints.baseURL + Endpoints.section;
 
 // get all users with token
 const getAllNotifications = () => {
-    return axios.get(API_URL + Endpoints.routes.locationsIndex, { headers: Endpoints.authHeader() });
+    return axios.get(API_URL + Endpoints.routes.notificationsIndex, { headers: Endpoints.authHeader() });
 };
 
 // add notification: code coming up
+const addNotification = (obj) => {
+    if(obj) {
+
+    }
+    return 'Error';
+}
 
 // get single user profile with token
 const getSingleNotification = (id) => {
-    return axios.get(API_URL + Endpoints.routes.locationsIndex + `/${id}`, { headers: Endpoints.authHeader() });
+    return axios.get(API_URL + Endpoints.routes.notificationsIndex + `/${id}`, { headers: Endpoints.authHeader() });
 };
 
-const locationsService = {
-  getAllNotifications,
-  getSingleNotification
+
+
+// add notification: code coming up
+const updateNotification = (obj) => {
+    if(obj) {
+
+    }
+    return 'Error';
+}
+
+
+
+// add notification: code coming up
+const deleteNotification = (obj) => {
+    if(obj) {
+
+    }
+    return 'Error';
+}
+
+const notificationsService = {
+    getAllNotifications,
+    getSingleNotification,
+    addNotification,
+    updateNotification,
+    deleteNotification
 };
 
-export default locationsService;
+export default notificationsService;
